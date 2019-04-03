16427be8dbf8bb72ff4326ce360eb200200adb94
velez8 <43214387+velez8@users.noreply.github.com>
2019-04-03 09:58:01 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $("button").click(function(){
     var amount = $(".amount").val();
     
     // 2. Use the input text to build the fortune to display.
-    var myAnswer = "By the year 2025"+ name + food + amount;
+    var myAnswer = "By the year 2025 "+ name + " " + food + parseInt(amount);
     $(".answer").text(myAnswer);
     
     // 3. Clear the old text from the inputs.
