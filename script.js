092933095f8bf20e064bcb051d287056d4c8696d
velez8 <43214387+velez8@users.noreply.github.com>
2019-04-03 09:54:25 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$("button").click(function(){
+    // 1. Extract the text from the inputs
+    var name = $(".name").val();
+    var food = $(".food").val();
+    var amount = $(".amount").val();
+    
+    // 2. Use the input text to build the fortune to display.
+    var myAnswer = "By the year 2025  name + food + amount;
+    $(".answer").text(myAnswer);
+    
+    // 3. Clear the old text from the inputs.
+    $(".name").val("");
+});
